53c0f0b4f5132f1187aa3b524191d2f23796739f
Batuhan Yalçıntürk <batuhanyalcinturk@gmail.com>
2023-04-18 15:20:44 +0300

    Revert "Blog Website Backend beginning"
    This reverts commit dfe9e21af0313d57227484257e908f4394b91b47.

--- blog-server/src/main/java/com/graysan/blog/repos/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.graysan.blog.repos;
-
-import com.graysan.blog.entities.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
--- blog-server/src/main/java/com/graysan/blog/request/CommentUpdateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.graysan.blog.request;
-
-import lombok.Data;
-
-@Data
-public class CommentUpdateRequest {
-
-    String text;
-}
