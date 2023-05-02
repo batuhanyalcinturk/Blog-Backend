b6bf08d26cdbebc475f1f398c7dbdc6212dbf2e6
Batuhan Yalçıntürk <batuhanyalcinturk@gmail.com>
2023-05-02 16:10:43 +0300

    DB güncelleme

--- src/main/java/com/graysan/blog/entities/Comment.java
+++ src/main/java/com/graysan/blog/entities/Comment.java
@@ -1,14 +1,7 @@
 package com.graysan.blog.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
@@ -19,6 +12,7 @@ import org.hibernate.annotations.OnDeleteAction;
 public class Comment {
 
     @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
     Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/com/graysan/blog/entities/Like.java
+++ src/main/java/com/graysan/blog/entities/Like.java
@@ -1,12 +1,7 @@
 package com.graysan.blog.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
@@ -17,7 +12,9 @@ import org.hibernate.annotations.OnDeleteAction;
 public class Like {
 
     @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
     Long id;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="post_id", nullable=false)
     @OnDelete(action = OnDeleteAction.CASCADE)
