10fee379dc4caa4b93adbfd4b8b40bfec067f91c
Batuhan Yalçıntürk <batuhanyalcinturk@gmail.com>
2023-05-15 22:04:09 +0300

    jwt token

---
+++ src/main/java/com/graysan/blog/service/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.graysan.blog.service;
+
+import com.graysan.blog.entities.User;
+import com.graysan.blog.repos.UserRepository;
+import com.graysan.blog.security.JwtUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username);
+        return JwtUserDetails.create(user);
+    }
+
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).get();
+        return JwtUserDetails.create(user);
+    }
+}
